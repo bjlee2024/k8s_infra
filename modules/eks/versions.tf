e351e0d130da56a5c24819a71427b4b0b54c98df
bjlee <axlgilby@gmail.com>
2024-09-13 03:40:22 +0000

    Add eks module-1

---
+++ modules/eks/versions.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_version = "~>1.9.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.64.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.5.2"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~>3.2.3"
+    }
+  }
+}
+
