a3c378aef86a58e00c3324fdc1f24230c8c1fdc0
bjlee <axlgilby@gmail.com>
2024-09-13 17:29:09 +0000

    updates

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -28,29 +28,6 @@ variable "private_subnet_ids" {
   description = "The private subnet IDs for the EKS nodes."
 }
 
-# variable "vpc_cidr_block" {
-#   type        = string
-#   default     = "10.0.0.0/16"
-#   description = "The CIDR block for the VPC."
-# }
-
-# variable "private_subnet_cidrs" {
-#   type        = list(string)
-#   default     = ["10.0.192.0/24", "10.0.193.0/24", "10.0.194.0/24"]
-#   description = "The CIDR blocks for the private subnets, default is 3 subnets in the VPC_CIDR_BLOCK whcich has 256 addresses each."
-# }
-
-# variable "public_subnet_cidrs" {
-#   type        = list(string)
-#   default     = ["10.0.0.0/18", "10.0.64.0/18", "10.0.128.0/18"]
-#   description = "The CIDR blocks for the public subnets, default is 3 subnets in the VPC_CIDR_BLOCK whcich has 16382 addresses each."
-# }
-
-# variable "availability_zones" {
-#   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#   description = "The availability zones to create subnets in, should be input where calling this module"
-# }
-
 variable "instance_types" {
   type        = list(string)
   default     = ["t3.medium"]
